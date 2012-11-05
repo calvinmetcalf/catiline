3b826c29a87ef55dbadb4fdb907d36eb1b548b95
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-11-04 19:44:26 -0500

    maybe make it like common or something

--- worker.js
+++ worker.js
@@ -1,1 +1,1 @@
-funcs={};function cWorker(name, func){	funcs[name]=func;};cWorker('_addLib',function(url){importScripts(url);return 'Success';});self.onmessage = function(event) {	var name =event.data[0];	var p = event.data[1];	var func = funcs[event.data[0]];	var cb = event.data[2]  self.postMessage([cb,func(p)]);};
+exports={};exports._addLib=function(url){importScripts(url);return 'Success';};self.onmessage = function(event) {	var name =event.data[0];	var p = event.data[1];	var func = exports[event.data[0]];	var cb = event.data[2]  self.postMessage([cb,func(p)]);};
