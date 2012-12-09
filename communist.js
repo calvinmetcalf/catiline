481d272b4982af78621568a188551ae5847f3531
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-12-09 08:13:52 -0500

    cleaning up some errors

--- communist.js
+++ communist.js
@@ -2,20 +2,20 @@ var communist = function(fun){
     if(typeof fun === "function"){
         window.URL = window.URL || window.webkiURL;
         var func = fun.toString();
-        var body = "var f = " + func + "self.addEventListener('message', function(e) {self.postMessage(f(data.e))});
+        var body = "var f = " + func + "self.addEventListener('message', function(e) {self.postMessage(f(data.e))})";
         var blob = new Blob([body]);
-        var bUrl = = window.URL.createObjectURL(blob);
-        _worker = new Worker(bUrl);
+        var bUrl = window.URL.createObjectURL(blob);
+        var _worker = new Worker(bUrl);
         this.prototype.send = function(data,cb){
         _worker.postMessage(data);
   
-             worker.onmessage = function(e){cb(e.data)};
+             _worker.onmessage = function(e){cb(e.data)};
              return;
        
         
-        }
+        };
         this.prototype.close = function(){
-            _worker.terminate()
-        }
+            _worker.terminate();
+        };
     }
-}
+};
