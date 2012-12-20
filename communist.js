fe913f30f99eb1286e70aef62067d3bfd468ce19
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-12-20 12:39:44 -0500

    updates

--- communist.js
+++ communist.js
@@ -1,21 +1,53 @@
-var Communist = function(fun){
-    if(typeof fun === "function"){
-        window.URL = window.URL || window.webkiURL;
-        var func = fun.toString();
-        var body = "var f = " + func + ";self.addEventListener('message', function(e) {self.postMessage(f(e.data))})";
-        var blob = new Blob([body], { "type" : "text\/javascript" });
-        var bUrl = window.URL.createObjectURL(blob);
-        var _worker = new Worker(bUrl);
-        this.send = function(data,cb){
-        _worker.postMessage(data);
-  
-             _worker.onmessage = function(e){cb(e.data)};
-             return;
-       
-        
-        };
-        this.close = function(){
-            _worker.terminate();
-        };
+(function() {
+  var Communist, communist,
+    __slice = [].slice;
+
+  Communist = function(fun) {
+    var bUrl, blob, body, func, _func, _worker;
+    if (typeof fun !== "function") {
+      return;
     }
-};
+    if (Worker) {
+      window.URL = window.URL || window.webkiURL;
+      func = fun.toString();
+      body = "var f = " + func + ";self.addEventListener('message', function(e) {self.postMessage(f.apply(null, e.data))})";
+      blob = new Blob([body], {
+        type: "text/javascript"
+      });
+      bUrl = window.URL.createObjectURL(blob);
+      _worker = new Worker(bUrl);
+      this.send = function() {
+        var cb, data, _i;
+        data = 2 <= arguments.length ? __slice.call(arguments, 0, _i = arguments.length - 1) : (_i = 0, []), cb = arguments[_i++];
+        _worker.postMessage(data);
+        _worker.onmessage = function(e) {
+          cb(e.data);
+          return true;
+        };
+        return true;
+      };
+      this.close = function() {
+        _worker.terminate();
+        return true;
+      };
+      return true;
+    } else {
+      _func = fun;
+      this.send = function() {
+        var cb, data, _i;
+        data = 2 <= arguments.length ? __slice.call(arguments, 0, _i = arguments.length - 1) : (_i = 0, []), cb = arguments[_i++];
+        cb(_func.apply(null, data));
+        return true;
+      };
+      this.close = function() {
+        return true;
+      };
+      return true;
+    }
+  };
+
+  communist = function(fun) {
+    return new Communist(fun);
+  };
+
+}).call(this);
