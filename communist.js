a81736e440c16faa6e9315c5d3a70309d8e0bc78
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-12-27 10:04:19 -0500

    updated js build

--- communist.js
+++ communist.js
@@ -3,25 +3,37 @@
     __slice = [].slice;
 
   Communist = function(fun) {
-    var bUrl, blob, body, func, _func, _worker;
+    var bUrl, blob, body, _func, _worker,
+      _this = this;
     if (typeof fun !== "function") {
       return;
     }
     if (window.Worker) {
       window.URL = window.URL || window.webkiURL;
-      func = fun.toString();
-      body = "var f = " + func + ";self.addEventListener('message', function(e) {self.postMessage(f.apply(null, e.data))})";
+      body = "var f =  " + (fun.toString()) + ";self.addEventListener('message', function(e) {try{self.postMessage({body:f.apply(null, e.data.body),cb:e.data.cb})}catch(err){self.postMessege({error:err,cb:e.data.cb})}})";
       blob = new Blob([body], {
         type: "text/javascript"
       });
       bUrl = window.URL.createObjectURL(blob);
       _worker = new Worker(bUrl);
+      this.CBs = {};
       this.send = function() {
-        var cb, data, _i;
+        var cb, data, id, _i;
         data = 2 <= arguments.length ? __slice.call(arguments, 0, _i = arguments.length - 1) : (_i = 0, []), cb = arguments[_i++];
-        _worker.postMessage(data);
+        id = ("" + Math.random()).slice(2);
+        _this.CBs[id] = cb;
+        _worker.postMessage({
+          body: data,
+          cb: id
+        });
         _worker.onmessage = function(e) {
-          cb(null, e.data);
+          if (e.data.body) {
+            _this.CBs[e.data.cb](null, e.data.body);
+            delete _this.CBs[e.data.cb];
+          } else if (e.data.error) {
+            CBs[e.data.cb](e.data.error);
+            delete _this.CBs[e.data.cb];
+          }
           return true;
         };
         _worker.onerror = function(e) {
