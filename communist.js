5be8abd88b196f7cba59d286876d373e17650004
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-12-28 09:25:02 -0500

    build

--- communist.js
+++ communist.js
@@ -5,11 +5,8 @@
   Communist = function(fun) {
     var bUrl, blob, body, _worker,
       _this = this;
-    if (typeof fun !== "function") {
-      return;
-    }
     window.URL = window.URL || window.webkiURL;
-    body = "var send;var _f =  " + (fun.toString()) + ";self.addEventListener('message', function(_e) {send = function(data){self.postMessage({messege:data,cb:_e.data.cb})};try{self.postMessage({body:_f.apply(null, _e.data.body),cb:_e.data.cb})}catch(_err){self.postMessege({error:_err,cb:_e.data.cb})}})";
+    body = "var send;var _f =  " + fun + ";self.addEventListener('message', function(_e) {send = function(data){self.postMessage({messege:data,cb:_e.data.cb})};try{self.postMessage({body:_f.apply(null, _e.data.body),cb:_e.data.cb})}catch(_err){self.postMessege({error:_err,cb:_e.data.cb})}})";
     blob = new Blob([body], {
       type: "text/javascript"
     });
@@ -47,38 +44,37 @@
       _worker.terminate();
       return true;
     };
-    return this;
+    return true;
   };
 
   Socialist = function(fun) {
-    var _func, _send;
-    _func = fun;
-    _send = function() {
+    eval("_func = " + fun);
+    this.send = function() {
       var cb, data, _i;
       data = 2 <= arguments.length ? __slice.call(arguments, 0, _i = arguments.length - 1) : (_i = 0, []), cb = arguments[_i++];
       window.send = function(m) {
-        return cb.apply(null, m);
+        return cb(null, m);
       };
       try {
-        cb(null, _func.apply(this, data));
+        cb(null, _func.apply(null, data));
       } catch (err) {
         cb(err);
       }
       return true;
     };
-    this.send = _send;
     this.close = function() {
+      var _func;
       _func = void 0;
       return true;
     };
-    return this;
+    return true;
   };
 
   window.communist = function(fun) {
     if (window.Worker) {
-      return new Communist(fun);
+      return new Communist(fun.toString());
     } else {
-      return new Socialist(fun);
+      return new Socialist(fun.toString());
     }
   };
 
