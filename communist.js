dec77052593aed0720d026cd12341c399e4f2c74
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-12-31 13:43:33 -0500

    merged in rpc

--- communist.js
+++ communist.js
@@ -1,7 +1,9 @@
 (function() {
-  var Communist, Socialist,
+  var Communist, Socialist, defaultFunc,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __slice = [].slice;
+
+  defaultFunc = "var _db = {}\n_db._add=function(name, func){\n	_db[name]=eval(\"(\"+func+\")\");\n	return true;	\n};	\n_db._rm=function(name){\n	delete _db[name];		\n	return true;\n};\n_db._test=function(a){\n	return a || \"all quiet\";\n};\nvar _f =  function () {\n    var args, method,__slice = [].slice;\n    method = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];\n    return _db[method].apply(null, args);\n  };\n self.addEventListener('message', function(_e) {\n 	self.send = function(data){\n 		self.postMessage({\n 			messege:data,cb:_e.data.cb\n 		});\n 	};\n	self.postMessage({\n 		cb:_e.data.cb,\n 		body:_f.apply(null, _e.data.body)\n 		});\n 	\n});";
 
   Communist = (function() {
 
@@ -10,7 +12,12 @@
 
       var bUrl, blob, body;
       window.URL = window.URL || window.webkiURL;
-      body = "var send;var _f =  " + fun + ";self.addEventListener('message', function(_e) {send = function(data){self.postMessage({messege:data,cb:_e.data.cb})};try{self.postMessage({body:_f.apply(null, _e.data.body),cb:_e.data.cb})}catch(_err){self.postMessege({error:_err,cb:_e.data.cb})}})";
+      if (fun) {
+        fun = fun.toString();
+        body = "var send;var _f =  " + fun + ";self.addEventListener('message', function(_e) {send = function(data){self.postMessage({message:data,cb:_e.data.cb})};self.postMessage({body:_f.apply(null, _e.data.body),cb:_e.data.cb})})";
+      } else {
+        body = defaultFunc;
+      }
       blob = new Blob([body], {
         type: "text/javascript"
       });
@@ -26,6 +33,11 @@
       var cb, data, id, _i,
         _this = this;
       data = 2 <= arguments.length ? __slice.call(arguments, 0, _i = arguments.length - 1) : (_i = 0, []), cb = arguments[_i++];
+      if (cb == null) {
+        cb = function() {
+          return true;
+        };
+      }
       id = ("" + Math.random()).slice(2);
       this.CBs[id] = cb;
       this._worker.postMessage({
@@ -36,11 +48,8 @@
         if (e.data.body) {
           _this.CBs[e.data.cb](null, e.data.body);
           delete _this.CBs[e.data.cb];
-        } else if (e.data.messege) {
-          _this.CBs[e.data.cb](null, e.data.messege);
-        } else if (e.data.error) {
-          CBs[e.data.cb](e.data.error);
-          delete _this.CBs[e.data.cb];
+        } else if (e.data.message) {
+          _this.CBs[e.data.cb](null, e.data.message);
         }
         return true;
       };
@@ -48,6 +57,26 @@
         cb(e);
         return true;
       };
+      return true;
+    };
+
+    Communist.prototype.add = function(method, func, cb) {
+      if (cb == null) {
+        cb = function() {
+          return true;
+        };
+      }
+      this.send("_add", method, func.toString(), cb);
+      return true;
+    };
+
+    Communist.prototype.remove = function(method, cb) {
+      if (cb == null) {
+        cb = function() {
+          return true;
+        };
+      }
+      this.send("_rm", method, cb);
       return true;
     };
 
@@ -68,6 +97,8 @@
 
     function Socialist(_func) {
       this._func = _func;
+      this.start = __bind(this.start, this);
+
       this.send = __bind(this.send, this);
 
     }
@@ -109,6 +140,10 @@
       return true;
     };
 
+    Socialist.prototype.start = function(cb) {
+      return this.send(cb);
+    };
+
     Socialist.prototype.close = function() {
       var _func;
       _func = void 0;
@@ -125,7 +160,11 @@
 
   window.communist = function(fun) {
     if (window.Worker) {
-      return new Communist(fun.toString());
+      if (fun) {
+        return new Communist(fun);
+      } else {
+        return new Communist();
+      }
     } else {
       return new Socialist(fun);
     }
