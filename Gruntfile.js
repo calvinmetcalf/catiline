334dc7c7840f2042dd51e7bc146e09c4796d53c3
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-05-30 08:33:21 -0400

    add linting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,70 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      browser: {
-        options:{
-          banner:'/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>*/\n/*!(c)2013 Calvin Metcalf @license MIT https://github.com/calvinmetcalf/communist */\n/*!Includes Promiscuous (c)2013 Ruben Verborgh @license MIT https://github.com/RubenVerborgh/promiscuous*/\n/*!Includes Material from setImmediate Copyright (c) 2012 Barnesandnoble.com, llc, Donavon West, and Domenic Denicola @license MIT https://github.com/NobleJS/setImmediate */\n',
-          mangle: {
-            except: ['Communist']
-          }
-        },
-        src: 'dist/<%= pkg.name %>.js',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    concat: {
-     
-      browser: { 
-      	options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>*/\n/*!©2013 Calvin Metcalf @license MIT https://github.com/calvinmetcalf/communist */\n',
-          seperator:";\n",
-          footer : '})();}'
-        },
-        files: {'dist/<%= pkg.name %>.js':['src/IE.js','src/setImmediate.js','src/promiscuous.js','src/all.js','src/utils.js','src/worker.single.js','src/worker.general.js','src/worker.multiuse.js','src/worker.object.js','src/queue.js','src/queue.dumb.js','src/worker.reducer.js','src/mapreduce.incremental.js','src/mapreduce.nonincremental.js','src/wrapup.js']}
-      }
-    },mocha_phantomjs: {
-    all: {
-      options: {
-        urls: [
-          "http://localhost:8000/test/index.html",
-          "http://localhost:8000/test/index.min.html"
-        ]
-      }
-    }
-  },
-    connect: {
-      server: {
-        options: {
-          port: 8000,
-          base: '.'
-        }
-      }
-    }
-  });
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			browser: {
+				options:{
+					banner:'/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>*/\n/*!(c)2013 Calvin Metcalf @license MIT https://github.com/calvinmetcalf/communist */\n/*!Includes Promiscuous (c)2013 Ruben Verborgh @license MIT https://github.com/RubenVerborgh/promiscuous*/\n/*!Includes Material from setImmediate Copyright (c) 2012 Barnesandnoble.com, llc, Donavon West, and Domenic Denicola @license MIT https://github.com/NobleJS/setImmediate */\n',
+					mangle: {
+						except: ['Communist']
+					}
+				},
+				src: 'dist/<%= pkg.name %>.js',
+				dest: 'dist/<%= pkg.name %>.min.js'
+			}
+		},
+		concat: {
+		 
+			browser: { 
+				options: {
+					banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>*/\n/*!©2013 Calvin Metcalf @license MIT https://github.com/calvinmetcalf/communist */\n',
+					seperator:";\n",
+					footer : '})();}'
+				},
+				files: {'dist/<%= pkg.name %>.js':['src/IE.js','src/setImmediate.js','src/promiscuous.js','src/all.js','src/utils.js','src/worker.single.js','src/worker.general.js','src/worker.multiuse.js','src/worker.object.js','src/queue.js','src/queue.dumb.js','src/worker.reducer.js','src/mapreduce.incremental.js','src/mapreduce.nonincremental.js','src/wrapup.js']}
+			}
+		},mocha_phantomjs: {
+		all: {
+			options: {
+				urls: [
+					"http://localhost:8000/test/index.html",
+					"http://localhost:8000/test/index.min.html"
+				]
+			}
+		}
+	},
+		connect: {
+			server: {
+				options: {
+					port: 8000,
+					base: '.'
+				}
+			}
+		},
+	jshint: {
+		options:{
+			multistr:true,
+			evil:true,
+			expr:true
+		},
+		beforeconcat: ['src/*.js'],
+		afterconcat: ['dist/communist.js']
+	}
+	});
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
  grunt.loadNpmTasks('grunt-contrib-concat');
  grunt.loadNpmTasks('grunt-simple-mocha');
  grunt.loadNpmTasks('grunt-contrib-connect');
  grunt.loadNpmTasks('grunt-mocha-phantomjs');
-  // Default task(s).
-  grunt.registerTask('browser',['concat:browser','uglify:browser']);
+ grunt.loadNpmTasks('grunt-contrib-jshint');
+	// Default task(s).
+	grunt.registerTask('browser',['concat:browser','uglify:browser']);
+	grunt.registerTask('lint',['jshint:afterconcat']);
 grunt.registerTask('test', ['connect', 'mocha_phantomjs']);
-  grunt.registerTask('default', ['browser','test']);
+	grunt.registerTask('default', ['browser','lint','test']);
 
 };
