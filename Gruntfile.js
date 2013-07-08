6facd6997434a40f818fe395d98bf10d077982d4
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-07-08 10:56:59 -0400

    is it a local issue to the leg one

--- Gruntfile.js
+++ Gruntfile.js
@@ -180,7 +180,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-saucelabs');
 	grunt.registerTask('template',templateThings);
-	grunt.registerTask('sauce',['server','saucelabs-mocha:legacy','saucelabs-mocha:shim','saucelabs-mocha:big']);
+	grunt.registerTask('sauce',['server','saucelabs-mocha:shim','saucelabs-mocha:big','saucelabs-mocha:legacy']);
 	grunt.registerTask('server',['connect']);
 	grunt.registerTask('browser',['concat:browser','uglify:browser']);
 	grunt.registerTask('lint',['jshint:afterconcat']);
