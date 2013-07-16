678d5e281899fffc26493204244136a15dcaee31
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-07-16 09:34:11 -0400

    single interms of object

---
+++ src/single.js
@@ -0,0 +1,33 @@
+//special case of worker only being called once, instead of sending the data
+//we can bake the data into the worker when we make it.
+
+function single(fun,data){
+	if(typeof Worker === 'undefined'||typeof fakeLegacy !== 'undefined'){
+		return multiUse(fun).data(data);
+	}
+	var promise = c.deferred();
+	var obj = {
+		fun:fun,
+		data:JSON.stringify(data),
+		init:function(){
+			var that = this;
+			var data = JSON.parse(this.data);
+			var cb = function(data,trans){
+				that.fire('done',data,trans);
+				self.terminate();
+			};
+			var resp = that.fun(data,cb);
+			if(typeof resp !== 'undefined'){
+				cb(resp);
+			}
+		}
+	};
+	var worker = object(obj);
+	worker.on('done',function(e){
+		promise.resolve(e);
+	});
+	worker.on('error',function(e){
+		promise.reject(e);
+	});
+	return promise.promise;
+}
