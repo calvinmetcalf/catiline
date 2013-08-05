018caf7a948a7b28349869005d167342f5cd2211
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-08-05 12:28:23 -0400

    update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,6 @@ module.exports = function(grunt) {
 			}).replace(/\n/gm,'')+"']";
 			var out = parent.replace(/\$\$fObj\$\$/,replacedChild);
 			grunt.file.write('./src/temp.js',out);
-			parent =  grunt.file.read('./src/iFrame.js');
-			child = grunt.file.read('./src/iWorker.js');
-			replacedChild = "['"+child.replace(/\$\$(.+?)\$\$/,function(a,b){
-				return "',"+b+",'";
-			}).replace(/\n/gm,'')+"']";
-			out = parent.replace(/\$\$fObj\$\$/,replacedChild);
-			grunt.file.write('./src/iTemp.js',out);
 	};
 	// Project configuration.
 	grunt.initConfig({
@@ -38,7 +31,7 @@ module.exports = function(grunt) {
 					seperator:";\n",
 					footer : 'communist.version = "<%= pkg.version %>";\n})(this);}'
 				},
-				files: {'dist/<%= pkg.name %>.js':['src/IE.js','src/setImmediate.js','src/promiscuous.js','src/utils.js','src/fakeWorker.js','src/iTemp.js','src/temp.js','src/queue.js','src/wrapup.js']}
+				files: {'dist/<%= pkg.name %>.js':['src/IE.js','src/setImmediate.js','src/promiscuous.js','src/utils.js','src/temp.js','src/queue.js','src/wrapup.js']}
 			}
 		},
 		mocha_phantomjs: {
