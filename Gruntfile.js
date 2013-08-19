ee8c75b0b9a0ed693ad8b0f78f07b5968783d0fd
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-08-19 16:11:02 -0400

    not using grunt defs

--- Gruntfile.js
+++ Gruntfile.js
@@ -183,7 +183,6 @@ module.exports = function(grunt) {
 		}
 	}
 	});
-	grunt.loadNpmTasks('grunt-defs');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-connect');
