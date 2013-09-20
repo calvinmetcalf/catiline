f504ac44ba0800ee79c1b6400432e97eadbd8f9a
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-09-20 10:08:59 -0400

    vbump

--- src/nextTick.js
+++ src/nextTick.js
@@ -1,21 +1,35 @@
 //lifted mostly from when
 //https://github.com/cujojs/when/
 let nextTick;
+const MutationObserver = global.MutationObserver || global.WebKitMutationObserver;
 if (typeof setImmediate === 'function') {
-    nextTick = setImmediate.bind(global,drainQueue);
+	nextTick = setImmediate.bind(global,drainQueue);
+}else if(MutationObserver){
+	let observer = new MutationObserver(drainQueue);
+	const element = document.createElement('div');
+	observer.observe(element, { attributes: true });
+
+	// Chrome Memory Leak: https://bugs.webkit.org/show_bug.cgi?id=93661
+	addEventListener('unload', function () {
+		observer.disconnect();
+		observer = null;
+	}, false);
+	nextTick =   function () {
+		element.setAttribute('drainQueue', 'drainQueue');
+	};
 }else{
-    const codeWord = 'com.catiline.setImmediate' + Math.random();
-    addEventListener('message', function (event) {
-        // This will catch all incoming messages (even from other windows!), so we need to try reasonably hard to
-        // avoid letting anyone else trick us into firing off. We test the origin is still this window, and that a
-        // (randomly generated) unpredictable identifying prefix is present.
-        if (event.source === window && event.data === codeWord) {
-            drainQueue();
-        }
-    }, false);
-    nextTick =  function() {
-        postMessage(codeWord, '*');
-    };
+	const codeWord = 'com.catiline.setImmediate' + Math.random();
+	addEventListener('message', function (event) {
+		// This will catch all incoming messages (even from other windows!), so we need to try reasonably hard to
+		// avoid letting anyone else trick us into firing off. We test the origin is still this window, and that a
+		// (randomly generated) unpredictable identifying prefix is present.
+		if (event.source === window && event.data === codeWord) {
+			drainQueue();
+		}
+	}, false);
+	nextTick =  function() {
+		postMessage(codeWord, '*');
+	};
 }
 let mainQueue = [];
 
@@ -25,9 +39,9 @@ let mainQueue = [];
  * @param {function} task
  */
 catiline.nextTick = function(task) {
-    if (mainQueue.push(task) === 1) {
-        nextTick();
-    }
+	if (mainQueue.push(task) === 1) {
+		nextTick();
+	}
 };
 
 /**
@@ -36,13 +50,13 @@ catiline.nextTick = function(task) {
  * processing until it is truly empty.
  */
 function drainQueue() {
-    let i = 0;
-    let task;
-    const innerQueue = mainQueue;
-    mainQueue = [];
+	let i = 0;
+	let task;
+	const innerQueue = mainQueue;
+	mainQueue = [];
 	/*jslint boss: true */
-    while (task = innerQueue[i++]) {
-        task();
-    }
+	while (task = innerQueue[i++]) {
+		task();
+	}
 
 }
