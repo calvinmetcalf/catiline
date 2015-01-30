2e96e8150ad9677993241f68e5cec483d092019e
Calvin Metcalf <cmetcalf@appgeo.com>
2015-01-30 14:46:58 -0500

    just use immediate

--- dist/catiline.js
+++ dist/catiline.js
@@ -1,4 +1,4 @@
-/*! catiline 2.9.3 2013-10-25*/
+/*! catiline 2.9.3 2015-01-30*/
 /*!©2013 Calvin Metcalf @license MIT https://github.com/calvinmetcalf/catiline */
 if (typeof document === 'undefined') {
 	self._noTransferable=true;
@@ -9,70 +9,138 @@ if (typeof document === 'undefined') {
 } else {
 (function(global){
 	'use strict';
-//overall structure based on when
-//https://github.com/cujojs/when/blob/master/when.js#L805-L852
-var nextTick;
-var MutationObserver = global.MutationObserver || global.WebKitMutationObserver;
-/*if (typeof setImmediate === 'function') {
-	nextTick = setImmediate.bind(global,drainQueue);
-}else */if(MutationObserver){
-	//based on RSVP
-	//https://github.com/tildeio/rsvp.js/blob/master/lib/rsvp/async.js
-	var observer = new MutationObserver(drainQueue);
-	var element = document.createElement('div');
-	observer.observe(element, { attributes: true });
-
-	// Chrome Memory Leak: https://bugs.webkit.org/show_bug.cgi?id=93661
-	addEventListener('unload', function () {
-		observer.disconnect();
-		observer = null;
-	}, false);
-	nextTick =   function () {
-		element.setAttribute('drainQueue', 'drainQueue');
-	};
-}else{
-	var codeWord = 'com.catiline.setImmediate' + Math.random();
-	addEventListener('message', function (event) {
-		// This will catch all incoming messages (even from other windows!), so we need to try reasonably hard to
-		// avoid letting anyone else trick us into firing off. We test the origin is still this window, and that a
-		// (randomly generated) unpredictable identifying prefix is present.
-		if (event.source === window && event.data === codeWord) {
-			drainQueue();
-		}
-	}, false);
-	nextTick =  function() {
-		postMessage(codeWord, '*');
-	};
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+'use strict';
+var types = [
+  _dereq_('./nextTick'),
+  _dereq_('./mutation.js'),
+  _dereq_('./messageChannel'),
+  _dereq_('./stateChange'),
+  _dereq_('./timeout')
+];
+var draining;
+var queue = [];
+//named nextTick for less confusing stack traces
+function nextTick() {
+  draining = true;
+  var i, oldQueue;
+  var len = queue.length;
+  while (len) {
+    oldQueue = queue;
+    queue = [];
+    i = -1;
+    while (++i < len) {
+      oldQueue[i]();
+    }
+    len = queue.length;
+  }
+  draining = false;
 }
-var mainQueue = [];
+var scheduleDrain;
+var i = -1;
+var len = types.length;
+while (++ i < len) {
+  if (types[i] && types[i].test && types[i].test()) {
+    scheduleDrain = types[i].install(nextTick);
+    break;
+  }
+}
+module.exports = immediate;
+function immediate(task) {
+  if (queue.push(task) === 1 && !draining) {
+    scheduleDrain();
+  }
+}
+},{"./messageChannel":2,"./mutation.js":3,"./nextTick":7,"./stateChange":4,"./timeout":5}],2:[function(_dereq_,module,exports){
+(function (global){
+'use strict';
 
-/**
- * Enqueue a task. If the queue is not currently scheduled to be
- * drained, schedule it.
- * @param {function} task
- */
-catiline.nextTick = function(task) {
-	if (mainQueue.push(task) === 1) {
-		nextTick();
-	}
+exports.test = function () {
+  if (global.setImmediate) {
+    // we can only get here in IE10
+    // which doesn't handel postMessage well
+    return false;
+  }
+  return typeof global.MessageChannel !== 'undefined';
 };
 
-/**
- * Drain the handler queue entirely, being careful to allow the
- * queue to be extended while it is being processed, and to continue
- * processing until it is truly empty.
- */
-function drainQueue() {
-	var i = 0;
-	var task;
-	var innerQueue = mainQueue;
-	mainQueue = [];
-	/*jslint boss: true */
-	while (task = innerQueue[i++]) {
-		task();
-	}
+exports.install = function (func) {
+  var channel = new global.MessageChannel();
+  channel.port1.onmessage = func;
+  return function () {
+    channel.port2.postMessage(0);
+  };
+};
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}],3:[function(_dereq_,module,exports){
+(function (global){
+'use strict';
+//based off rsvp https://github.com/tildeio/rsvp.js
+//license https://github.com/tildeio/rsvp.js/blob/master/LICENSE
+//https://github.com/tildeio/rsvp.js/blob/master/lib/rsvp/asap.js
 
-}
+var Mutation = global.MutationObserver || global.WebKitMutationObserver;
+
+exports.test = function () {
+  return Mutation;
+};
+
+exports.install = function (handle) {
+  var called = 0;
+  var observer = new Mutation(handle);
+  var element = global.document.createTextNode('');
+  observer.observe(element, {
+    characterData: true
+  });
+  return function () {
+    element.data = (called = ++called % 2);
+  };
+};
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}],4:[function(_dereq_,module,exports){
+(function (global){
+'use strict';
+
+exports.test = function () {
+  return 'document' in global && 'onreadystatechange' in global.document.createElement('script');
+};
+
+exports.install = function (handle) {
+  return function () {
+
+    // Create a <script> element; its readystatechange event will be fired asynchronously once it is inserted
+    // into the document. Do so, thus queuing up the task. Remember to clean up once it's been called.
+    var scriptEl = global.document.createElement('script');
+    scriptEl.onreadystatechange = function () {
+      handle();
+
+      scriptEl.onreadystatechange = null;
+      scriptEl.parentNode.removeChild(scriptEl);
+      scriptEl = null;
+    };
+    global.document.documentElement.appendChild(scriptEl);
+
+    return handle;
+  };
+};
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}],5:[function(_dereq_,module,exports){
+'use strict';
+exports.test = function () {
+  return true;
+};
+
+exports.install = function (t) {
+  return function () {
+    setTimeout(t, 0);
+  };
+};
+},{}],6:[function(_dereq_,module,exports){
+catiline.nextTick = _dereq_('immediate');
+},{"immediate":1}],7:[function(_dereq_,module,exports){
+
+},{}]},{},[6]);
+
 
 var func = 'function';
 // Creates a deferred: an object with a promise and corresponding resolve/reject methods
@@ -348,7 +416,7 @@ function makeFallbackWorker(script){
 //accepts an array of strings, joins them, and turns them into a worker.
 catiline.makeWorker = function (strings, codeword){
 	if(!catiline._hasWorker){
-		return catiline.makeIWorker(strings,codeword);
+		return catiline.makeIWorker(strings, codeword);
 	}
 	var worker;
 	var script = moveImports(strings.join('\n'),'\');\n');
@@ -361,7 +429,7 @@ catiline.makeWorker = function (strings, codeword){
 		try{
 			worker=makeFallbackWorker(script);
 		}catch(ee){
-			worker = catiline.makeIWorker(strings,codeword);
+			worker = catiline.makeIWorker(strings, codeword);
 		}
 	}finally{
 		return worker;
